8b84d4f69bc8c88d452c80f5f10430f2f37daaed
Mahe077 <2019cs077@stu.ucsc.lk>
2022-09-18 19:44:50 +0530

    update item route

--- src/routers/item.js
+++ src/routers/item.js
@@ -1,95 +1,107 @@
-const express = require('express')
-const Item = require('../models/item')
-const Auth = require('../middleware/auth')
+const express = require("express");
+const Item = require("../models/item");
+const Auth = require("../middleware/auth");
 
-const router = new express.Router()
+const router = new express.Router();
 
 //fetch all items
-router.get('/items', async(req, res) => {
-    if(req.query.user == 1) {
-        try {
-           const items = await Item.find({ owner: req.userId})
-            res.status(200).send(items)
-        } catch (error) {
-            console.log(error)
-            res.status(500).send('something went wrong')
-        }
-    } else {
+router.get("/items", async (req, res) => {
+  console.log(req.query);
+  if (req.query.user == "1") {
+    console.log(req.headers.userid);
     try {
-        const items = await Item.find({})
-        res.status(200).send(items)
+      const items = await Item.find({ owner: req.headers.userid });
+      res.status(200).send(items);
     } catch (error) {
-        res.status(400).send(error)
+      console.log(error);
+      res.status(500).send("something went wrong");
     }
-}
-})
+  } 
+  else if(req.body.name){
+    try {
+      const items = await Item.find({ name: req.body.name });
+      res.status(200).send(items);
+    } catch (error) {
+      res.status(400).send(error);
+    }
+  }
+  else {
+    try {
+      const items = await Item.find({});
+      res.status(200).send(items);
+    } catch (error) {
+      res.status(400).send(error);
+    }
+  }
+});
 
 //fetch an item
-router.get('/items/one', async(req, res) => {
-    try{
-        const item = await Item.findOne({_id: req.headers.id})
-        if(!item) {
-            res.status(404).send({error: "Item not found"})
-        }
-        res.status(200).send(item) 
-    } catch (error) {
-        res.status(400).send(error)
+router.get("/items/one", async (req, res) => {
+  try {
+    const item = await Item.findOne({ _id: req.headers.id });
+    if (!item) {
+      res.status(404).send({ error: "Item not found" });
     }
-})
+    res.status(200).send(item);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
 
 //create an item
-router.post('/items', async(req, res) => {
-    try {
-        const newItem = new Item({
-            ...req.body
-        })
-        await newItem.save()
-        res.status(201).send(newItem)
-    } catch (error) {
-        console.log({error})
-        res.status(400).send({message: "error"})
-    }
-})
+router.post("/items", async (req, res) => {
+  try {
+    const newItem = new Item({
+      ...req.body,
+    });
+    await newItem.save();
+    res.status(201).send(newItem);
+  } catch (error) {
+    console.log({ error });
+    res.status(400).send({ message: "error" });
+  }
+});
 
 //update an item
 
-router.patch('/items',  async(req, res) => {
-    const updates = Object.keys(req.body)
-    const allowedUpdates = ['name', 'description', 'category', 'price', 'image']
+router.patch("/items", async (req, res) => {
+  const updates = Object.keys(req.body);
+  const allowedUpdates = ["name", "description", "category", "price", "image"];
 
-    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+  const isValidOperation = updates.every((update) =>
+    allowedUpdates.includes(update)
+  );
 
-    if(!isValidOperation) {
-        return res.status(400).send({ error: 'invalid updates'})
+  if (!isValidOperation) {
+    return res.status(400).send({ error: "invalid updates" });
+  }
+
+  try {
+    const item = await Item.findOne({ _id: req.headers.id });
+
+    if (!item) {
+      return res.status(404).send();
     }
 
-    try {
-        const item = await Item.findOne({ _id: req.headers.id})
-    
-        if(!item){
-            return res.status(404).send()
-        }
-
-        updates.forEach((update) => item[update] = req.body[update])
-        await item.save()
-        res.send(item)
-    } catch (error) {
-        res.status(400).send(error)
-    }
-})
+    updates.forEach((update) => (item[update] = req.body[update]));
+    await item.save();
+    res.send(item);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
 
 //delete item
-router.delete('/items', async(req, res) => {
-    try {
-        const deletedItem = await Item.findOneAndDelete( {_id: req.headers.id} )
-        if(!deletedItem) {
-            res.status(404).send({error: "Item not found"})
-        }
-        res.send(deletedItem)
-    } catch (error) {
-        res.status(400).send(error)
+router.delete("/items", async (req, res) => {
+  try {
+    const deletedItem = await Item.findOneAndDelete({ _id: req.headers.id });
+    if (!deletedItem) {
+      res.status(404).send({ error: "Item not found" });
     }
-})
+    res.send(deletedItem);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
 
-
-module.exports = router
+module.exports = router;
